9d5f189f24311822c7e2f83c36a4607eeb400e8b
zhiganov <tema.zhiganov@gmail.com>
2022-12-05 08:26:01 +0000

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Protein',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Protein</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Projects Playbook
       </span>
     </>
   ),
